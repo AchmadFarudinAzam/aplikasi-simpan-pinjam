ee8e5ac722c9882237a978ac2cf2c010b1629339
AchmadFarudinAzam <107410730+AchmadFarudinAzam@users.noreply.github.com>
2025-09-01 14:32:20 +0700

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->string('role'); // 'admin' or 'nasabah'
+            $table->string('jabatan')->nullable();
+            $table->string('telp')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
